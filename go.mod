d7dae9bf8c035ca80dd13b7c6c7a42b5dce202df
Erik Lindström <erik.lindstrom@zapote.se>
2020-06-17 23:15:07 +0200

    new mod

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
-module github.com/zapote/auth
+module github.com/zapote/apply-auth
 
 go 1.14
+
+require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/google/uuid v1.1.1
+)
--- roles.go
+++ roles.go
@@ -3,14 +3,12 @@ package auth
 import (
 	"log"
 	"net/http"
-
-	"bitbucket.org/zapote/apply-api/types"
 )
 
 //Roles authorizes correct roles in payload from context
 func Roles(fn http.HandlerFunc, roles ...string) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		pl := r.Context().Value(types.PayloadContextKey)
+		pl := r.Context().Value(PayloadContextKey)
 		log.Println(pl)
 		fn(w, r)
 	}
